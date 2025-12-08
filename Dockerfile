ea7c0ab27d762e02e30464fb6b43b317a034ec48
Michael Vera <michaelvera13@gmail.com>
2025-12-08 09:41:26 -0500

    cambios de dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,9 @@
-FROM node:22-alpine AS builder
-WORKDIR /app
-
-COPY package*.json ./
-RUN npm install
-
-COPY . .
-RUN npm run build
-
 FROM node:22-alpine AS runner
 WORKDIR /app
 
 ENV NODE_ENV=production
 ENV PORT=3000
+ENV HOST=0.0.0.0   # 👈 ESTA LÍNEA ES LA CLAVE
 
 COPY --from=builder /app/package*.json ./
 COPY --from=builder /app/node_modules ./node_modules
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
+
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import icon from 'astro-icon';
-
-const PORT = Number(process.env.PORT) || 3000;
 
 export default defineConfig({
   output: 'server',
@@ -12,9 +11,5 @@ export default defineConfig({
     mode: 'standalone',
   }),
   base: '/',
-  server: {
-    port: PORT,
-    host: '0.0.0.0',
-  },
   integrations: [sitemap(), tailwind(), icon()],
 });
