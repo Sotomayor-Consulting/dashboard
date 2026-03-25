87a1315997f70c2ef487524b2eafda572f900d27
Jefferson Rios <jeff_rios2002@outlook.com>
2026-03-25 17:04:37 -0500

    Agregar filtrado en Empresas

--- src/modules/companies/components/company-columns.tsx
+++ src/modules/companies/components/company-columns.tsx
@@ -1,6 +1,17 @@
 import type { ColumnDef } from '@tanstack/react-table';
 import * as Dictionaries from '../utils/dictionaries';
 import type { CompanyTableRow } from '../types';
+import { Button } from '@components/components/ui/button';
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@components/components/ui/dropdown-menu';
+
+import { Building, LogOutIcon, SettingsIcon, UserIcon } from 'lucide-react';
 
 export const columns: ColumnDef<CompanyTableRow>[] = [
 	{ accessorKey: 'legal_name', header: 'Nombre' },
@@ -36,4 +47,50 @@ export const columns: ColumnDef<CompanyTableRow>[] = [
 	},
 	{ accessorKey: 'formation_country', header: 'País' },
 	{ accessorKey: 'formation_state', header: 'Jurisdicción' },
+	{
+		id: 'actions',
+		cell: ({ row }) => {
+			const id = row.id;
+			return (
+				<div>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant="outline">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="1em"
+									height="1em"
+									viewBox="0 0 24 24"
+								>
+									<path
+										fill="currentColor"
+										d="m12 15l-4.243-4.242l1.415-1.414L12 12.172l2.828-2.828l1.415 1.414z"
+									/>
+								</svg>
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							<DropdownMenuItem>
+								<Building />
+								Profile
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<Building />
+								Billing
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<SettingsIcon />
+								Settings
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem variant="destructive">
+								<LogOutIcon />
+								Log out
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			);
+		},
+	},
 ];
--- src/modules/companies/components/company-table.tsx
+++ src/modules/companies/components/company-table.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 import {
 	type ColumnDef,
+	type ColumnFiltersState,
+	type SortingState,
 	flexRender,
+	getSortedRowModel,
+	getPaginationRowModel,
 	getCoreRowModel,
+	getFilteredRowModel,
 	useReactTable,
 } from '@tanstack/react-table';
+
 import {
 	Table,
 	TableBody,
@@ -12,6 +20,8 @@ import {
 	TableHeader,
 	TableRow,
 } from '@components/components/ui/table';
+
+import { Input } from '@components/components/ui/input';
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[];
@@ -22,56 +32,90 @@ export function CompanyTable<TData, TValue>({
 	columns,
 	data,
 }: DataTableProps<TData, TValue>) {
+	const [sorting, setSorting] = useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
 	const table = useReactTable({
 		data,
 		columns,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
 		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		state: {
+			sorting,
+			columnFilters,
+		},
 	});
 
 	return (
-		<div className="overflow-hidden rounded-md border">
-			<Table>
-				<TableHeader>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<TableRow key={headerGroup.id}>
-							{headerGroup.headers.map((header) => {
-								return (
-									<TableHead key={header.id}>
-										{header.isPlaceholder
-											? null
-											: flexRender(
-													header.column.columnDef.header,
-													header.getContext(),
-												)}
-									</TableHead>
-								);
-							})}
-						</TableRow>
-					))}
-				</TableHeader>
-				<TableBody>
-					{table.getRowModel().rows?.length ? (
-						table.getRowModel().rows.map((row) => (
-							<TableRow
-								key={row.id}
-								data-state={row.getIsSelected() && 'selected'}
-							>
-								{row.getVisibleCells().map((cell) => (
-									<TableCell key={cell.id}>
-										{flexRender(cell.column.columnDef.cell, cell.getContext())}
-									</TableCell>
-								))}
+		<div>
+			<div>
+				<div className="flex items-center py-4">
+					<Input
+						placeholder="Filtrar por nombre..."
+						value={
+							(table.getColumn('legal_name')?.getFilterValue() as string) ?? ''
+						}
+						onChange={(event) =>
+							table.getColumn('legal_name')?.setFilterValue(event.target.value)
+						}
+						className="max-w-sm"
+					/>
+				</div>
+			</div>
+			<div className="overflow-hidden rounded-md border">
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext(),
+													)}
+										</TableHead>
+									);
+								})}
 							</TableRow>
-						))
-					) : (
-						<TableRow>
-							<TableCell colSpan={columns.length} className="h-24 text-center">
-								No results.
-							</TableCell>
-						</TableRow>
-					)}
-				</TableBody>
-			</Table>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && 'selected'}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext(),
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center"
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
 		</div>
 	);
 }
