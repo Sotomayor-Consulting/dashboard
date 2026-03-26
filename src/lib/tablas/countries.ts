c81b72580622ad3c3295bf3355d37e0b36b0c664
Jefferson Rios <jeff_rios2002@outlook.com>
2026-03-26 17:43:38 -0500

    Cambio de nombre de tabla

---
+++ src/lib/tablas/countries.ts
@@ -0,0 +1,16 @@
+import type { SupabaseClient } from '@supabase/supabase-js';
+import type { Country } from '@modules/companies/types';
+
+export const getCountries = async (supabase: SupabaseClient) => {
+	const { data, error } = await supabase
+		.from('countries')
+		.select('*')
+		.order('name', { ascending: true });
+
+	if (error) {
+		console.error('Error fetching all países:', error);
+		throw error;
+	}
+
+	return data as Country[];
+};
