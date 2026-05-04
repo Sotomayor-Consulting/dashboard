f95edea378c41f399d7a9d594266308f942d0fc5
damocles99 <your.email@example.com>
2026-05-04 14:33:26 -0500

    union de formularios en una sola vista

--- src/lib/interfaz/itemsNavegacion.ts
+++ src/lib/interfaz/itemsNavegacion.ts
@@ -77,17 +77,7 @@ export const menuItems: MenuItem[] = [
 		group: 'Admin',
 		colors: 'text-violet-500'
 	},
-	{
-		id: 'verificacion-formulario-incorp',
-		label: 'Incorporación',
-		href: '/admin/verificacion/',
-		tooltip: 'Gestiona y verifica los datos del formulario de incorporación',
-		roles: ['admin'],
-		svgname: 'ri:archive-stack-line',
-		sequence: 110,
-		group: 'Admin',
-		colors: 'text-violet-500'
-	},
+
 	{
 		id: 'menu-vista-de-pagos',
 		label: 'Pagos',
@@ -129,17 +119,6 @@ export const menuItems: MenuItem[] = [
 		roles: ['admin'],
 		svgname: 'ri:file-edit-line',
 		sequence: 150,
-		group: 'Admin',
-		colors: 'text-violet-500'
-	},
-	{
-		id: 'crud-formularios-enviados',
-		label: 'Docs Enviados',
-		href: '/formularios/enviados/',
-		tooltip: 'Gestiona las respuestas y estados de documentos',
-		roles: ['admin'],
-		svgname: 'ri:file-check-line',
-		sequence: 160,
 		group: 'Admin',
 		colors: 'text-violet-500'
 	},
